e172fb2ce433d6107d468342bc2f850c8c4e1df3
daruban <v.romanova@t1-group.ru>
2026-04-16 13:26:43 +0300

    add middleware

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from .api import api_router
+from .middleware import ProcessTimeMiddleware
 
 
 app = FastAPI()
+app.add_middleware(ProcessTimeMiddleware)
 app.include_router(api_router)
